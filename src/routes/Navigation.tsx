73c7debbbc024bf80807474f4d56672826901b26
jantonio2 <jesusrochavedia@gmail.com>
2022-06-30 21:26:31 -0400

    Rutas dinámicas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,10 +2,12 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
+  NavLink,
+  Redirect
 } from 'react-router-dom';
 import logo from '../logo.svg';
 import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { routes } from './routes';
 
 export const Navigation = () => {
   return (
@@ -14,30 +16,36 @@ export const Navigation = () => {
         <nav>
           <img src={ logo } alt="React Logo" />
           <ul>
-            <li>
-              <NavLink to="/lazy1" activeClassName='nav-active' exact>Lazy1</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" activeClassName='nav-active' exact>Lazy2</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" activeClassName='nav-active' exact>Lazy3</NavLink>
-            </li>
+            {
+              routes.map( route => (
+                <li key={ route.to } >
+                  <NavLink
+                    to={ route.to } 
+                    activeClassName='nav-active' exact
+                  >
+                    { route.name }
+                  </NavLink>
+                </li>
+              ))
+            }
           </ul>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/lazy1">
-            <LazyPage1 />
-          </Route>
-          <Route path="/lazy2">
-            <LazyPage2 />
-          </Route>
-          <Route path="/lazy3">
-            <LazyPage3 />
-          </Route>
+          {
+            routes.map( route => (
+              <Route 
+                key={ route.path }
+                path={ route.path }
+                render={ () => <route.Component /> }
+              />
+            ))
+          }
+
+          <Redirect to={ routes[0].to } />
+
         </Switch>
       </div>
     </Router>
