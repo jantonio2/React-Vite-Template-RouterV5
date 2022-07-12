5a6178a2b340a618a72407c9da652d6b5c7e6118
jantonio2 <jesusrochavedia@gmail.com>
2022-07-12 19:21:32 -0400

    Aplicando Lazy load

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,48 +6,50 @@ import {
   Redirect
 } from 'react-router-dom';
 import logo from '../logo.svg';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 import { routes } from './routes';
+import { Suspense } from 'react';
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className='main-layout'>
-        <nav>
-          <img src={ logo } alt="React Logo" />
-          <ul>
+    <Suspense fallback={ <span> Loading... </span> }>
+      <Router>
+        <div className='main-layout'>
+          <nav>
+            <img src={ logo } alt="React Logo" />
+            <ul>
+              {
+                routes.map( route => (
+                  <li key={ route.to } >
+                    <NavLink
+                      to={ route.to } 
+                      activeClassName='nav-active' exact
+                    >
+                      { route.name }
+                    </NavLink>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
             {
               routes.map( route => (
-                <li key={ route.to } >
-                  <NavLink
-                    to={ route.to } 
-                    activeClassName='nav-active' exact
-                  >
-                    { route.name }
-                  </NavLink>
-                </li>
+                <Route 
+                  key={ route.path }
+                  path={ route.path }
+                  render={ () => <route.Component /> }
+                />
               ))
             }
-          </ul>
-        </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          {
-            routes.map( route => (
-              <Route 
-                key={ route.path }
-                path={ route.path }
-                render={ () => <route.Component /> }
-              />
-            ))
-          }
+            <Redirect to={ routes[0].to } />
 
-          <Redirect to={ routes[0].to } />
-
-        </Switch>
-      </div>
-    </Router>
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,19 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { lazy, LazyExoticComponent } from 'react';
+
 
 type JSXComponent = () => JSX.Element;
 
 interface Route {
   path: string;
   to: string;
-  Component: JSXComponent;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
   name: string;
   children?: Route[]
 }
+
+const LazyPage1 = lazy( () => import('../01-lazyload/pages/LazyPage1') );
+const LazyPage2 = lazy( () => import('../01-lazyload/pages/LazyPage2') );
+const LazyPage3 = lazy( () => import('../01-lazyload/pages/LazyPage3') );
 
 export const routes: Route[] = [
   {
