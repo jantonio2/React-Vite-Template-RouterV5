f977f73b8ec5600129aebe60d59ced1afd034e2d
jantonio2 <jesusrochavedia@gmail.com>
2022-07-12 20:06:24 -0400

    Cambiando de nombre a los chunks para webpack, vite aun por cambiar

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -11,9 +11,9 @@ interface Route {
   children?: Route[]
 }
 
-const LazyPage1 = lazy( () => import('../01-lazyload/pages/LazyPage1') );
-const LazyPage2 = lazy( () => import('../01-lazyload/pages/LazyPage2') );
-const LazyPage3 = lazy( () => import('../01-lazyload/pages/LazyPage3') );
+const LazyPage1 = lazy( () => import( /* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1') );
+const LazyPage2 = lazy( () => import( /* webpackChunkName: "LazyPage2adas" */ '../01-lazyload/pages/LazyPage2') );
+const LazyPage3 = lazy( () => import( /* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3') );
 
 export const routes: Route[] = [
   {
